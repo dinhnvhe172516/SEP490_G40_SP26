5a19a491bf36bffdbfea95c9bd01a1404f69342e
Tran Trung Toan <trantrungtoan17092003@gmail.com>
2026-04-08 10:21:10 +0700

    fix(email): change SMTP transport security setting to false

--- backend/src/common/service/email.service.js
+++ backend/src/common/service/email.service.js
@@ -7,7 +7,7 @@ class EmailService {
         this.transporter = nodemailer.createTransport({
             host: process.env.SMTP_HOST,
             port: process.env.SMTP_PORT,
-            secure: true,
+            secure: false,
             auth: {
                 user: process.env.SMTP_USER,
                 pass: process.env.SMTP_PASS
